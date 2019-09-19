8f2625b42cf64773f440a47f2b9746832d4cae95
Dimitris Exadaktylos <jimexa@yahoo.gr>
2019-09-19 19:51:01 +0300

    adjust

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mariannaki <code>src/App.js</code> and save to reload.
+          Mariannaki mou pou to agapw <code>src/App.js</code> and save to
+          reload.
         </p>
         <a
           className="App-link"
