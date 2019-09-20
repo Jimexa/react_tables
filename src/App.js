69e3412aa27d92cc88c24fd60bcb3d44dc461544
Dimitris Exadaktylos <jimexa@yahoo.gr>
2019-09-20 10:15:46 +0300

    add bootstrap's row and columns

--- src/App.js
+++ src/App.js
@@ -3,12 +3,47 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import TableScreen from "./components/tableScreen/TableScreen";
 
+const data = [
+  {
+    firstName: "Dimitris",
+    lastName: "Exadaktylos",
+    adress: "Trapezountiou",
+    jobTitle: "Front end developer"
+  },
+  {
+    firstName: "Marianna",
+    lastName: "Michala",
+    adress: "Trapezountiou",
+    jobTitle: "Arheologist"
+  },
+  {
+    firstName: "Nikos",
+    lastName: "Triantafyllou",
+    adress: "Mets",
+    jobTitle: "Marketeer"
+  },
+  {
+    firstName: "Kostas",
+    lastName: "Chobardas",
+    adress: "New Xalkidona",
+    jobTitle: "Psychiatrist"
+  },
+  {
+    firstName: "Afroditi",
+    lastName: "Kalogianni",
+    adress: "New Xalkidona",
+    jobTitle: "Veterinarian"
+  }
+];
+
 function App() {
   return (
     <body>
       <div className="App">
         <p>Hello..!</p>
-        <TableScreen />
+        {/* <div className="tableContainer"> */}
+        <TableScreen data={data} />
+        {/* </div> */}
       </div>
     </body>
   );
--- src/components/tableScreen/TableScreen.js
+++ src/components/tableScreen/TableScreen.js
@@ -1,45 +1,49 @@
 import React, { Component } from "react";
 import "./TableScreen.css";
+import Table from "react-bootstrap/Table";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class TableScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    let tableData = this.props.data.map(item => (
+      <tr>
+        <td>{item.firstName}</td>
+        <td>{item.lastName}</td>
+        <td>{item.adress}</td>
+        <td>{item.jobTitle}</td>
+      </tr>
+    ));
+
     return (
-      <table className="table">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Adress</th>
-            <th scope="col">Job Title</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Dimitris</td>
-            <td>Exadaktylos</td>
-            <td>Trapezountiou 4</td>
-            <td>Front end Developer</td>
-          </tr>
-          <tr>
-            <td>Dimitris</td>
-            <td>Exadaktylos</td>
-            <td>Trapezountiou 4</td>
-            <td>Front end Developer</td>
-          </tr>
-          <tr>
-            <td>Dimitris</td>
-            <td>Exadaktylos</td>
-            <td>Trapezountiou 4</td>
-            <td>Front end Developer</td>
-          </tr>
-          <tr>
-            <td>Dimitris</td>
-            <td>Exadaktylos</td>
-            <td>Trapezountiou 4</td>
-            <td>Front end Developer</td>
-          </tr>
-        </tbody>
-      </table>
+      <Row>
+        <Col xs={1}></Col>
+        <Col>
+          <Table
+            class="ali"
+            striped
+            bordered
+            hover
+            variant="dark"
+            responsive
+            className="table"
+          >
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Adress</th>
+                <th scope="col">Job Title</th>
+              </tr>
+            </thead>
+            <tbody>{tableData}</tbody>
+          </Table>
+        </Col>
+        <Col xs={1}></Col>
+      </Row>
     );
   }
 }
